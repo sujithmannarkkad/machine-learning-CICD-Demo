dcf59046032c9dabaedff6aef4fb4765932e901e
sujithmannarkkad <sujithmannarkkad@gmail.com>
2022-06-23 07:41:24 +0530

    text change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Starting machine learning project. This is a simple flask app"
+    return "CI-CD established"
 
 
 if __name__ == "__main__":
