6ba00aedaa036a2ce532c83f5f78c29b4bfcc6ed
sujithmannarkkad <sujithmannarkkad@gmail.com>
2022-06-23 11:39:05 +0530

    exception module

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        err=HousingException(e,sys)
+        logging.info(err)
+
     logging.info("we are in the main module")
     return "CI-CD established"
 
